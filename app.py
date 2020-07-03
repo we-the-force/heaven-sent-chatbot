f10e87ab0fe8fa4d1d4c65d5e782b6d2d0b50a36
srlechuga18 <aledeleon222@hotmail.com>
2020-07-03 15:36:14 -0500

    redistribution

--- app.py
+++ app.py
@@ -7,22 +7,25 @@ import json
 app = Flask(__name__)
 
 
-def _url(path):
-    return 'http://167.172.156.126:1337' + path
-
-
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Ahoy World!"
 
 
-@app.route("/test")
+@app.route("/user")
 def return_files():
-    res = requests.get(_url('/users/?mobile=8442590880'))
+    res = requests.get(_url('/users/?mobile=8442590888'))
     data = json.loads(res.text)
     #exist = len(data) > 0
     #name = data[0]['name'] if exist > 0 else 'No user'
-    return str(len(data))
+    return str(data)
+
+
+@app.route("/contact")
+def return_contacts():
+    res = requests.get(_url('/contacts?contact.id=28'))
+    data = json.loads(res.text)
+    return str(data)
 
 
 @app.route("/sms", methods=['POST'])
@@ -30,22 +33,13 @@ def sms_reply():
     #msg = request.form.get('Body')
     trasmisor = request.form.get('From')
     number = trasmisor.split(':')
-    parsed_phone = phonenumbers.parse(number[1], None)
-    national_phone = phonenumbers.format_number(
-        parsed_phone, phonenumbers.PhoneNumberFormat.NATIONAL).replace(" ","")
-    international_phone = phonenumbers.format_number(
-        parsed_phone, phonenumbers.PhoneNumberFormat.INTERNATIONAL).replace(" ","")
-    get_user = requests.get(_url('/users/?mobile={0}'.format(national_phone)))
-    user_json = json.loads(get_user.text)
-    print("No1 {0} \nNo2 {1}".format(national_phone,international_phone))
+    user_json = _get('/users/?mobile={0}'.format(_phone('NAT',number[1])))
     if len(user_json) <= 0:
-        get_user = requests.get(
-            _url('/users/?mobile={0}'.format(international_phone)))
-        user_json = json.loads(get_user.text)
+        user_json = _get('/users/?mobile={0}'.format(_phone('INT',number[1])))
         if len(user_json) <= 0:
             user_json = False
     if user_json:
-        message = ("It's a pleasure talking to you today {0}, how can I help you ?".format(
+        message = ("It's a pleasure talking to you {0}, how can I help you ?".format(
             user_json[0]['name']))
     else:
         message = (
@@ -54,6 +48,21 @@ def sms_reply():
     resp = MessagingResponse()
     resp.message(message)
     return str(resp)
+
+
+def _url(path):
+    return 'http://167.172.156.126:1337' + path
+
+
+def _get(path):
+    get_user = requests.get(_url(path))
+    return json.loads(get_user.text)
+
+
+def _phone(type, number):
+    parsed_phone = phonenumbers.parse(number, None)
+    format_phone = phonenumbers.PhoneNumberFormat.INTERNATIONAL if type == 'INT' else phonenumbers.PhoneNumberFormat.NATIONAL
+    return phonenumbers.format_number(parsed_phone, format_phone).replace(" ", "")
 
 
 if __name__ == "__main__":
