a168395c07678542dcf261e60e1e9b3ad628b857
srlechuga18 <aledeleon222@hotmail.com>
2020-07-06 17:58:22 -0500

    contact

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from twilio.twiml.messaging_response import Message, MessagingResponse
 import requests
 import phonenumbers
 import json
 
 app = Flask(__name__)
+app.secret_key = 'beZ}6EmDsU1RNICI!Q)4EWq%?_1/]h'
 
 
 @app.route("/")
@@ -20,28 +21,52 @@ def return_files():
     #name = data[0]['name'] if exist > 0 else 'No user'
     return str(data)
 
+@app.route("/session")
+def session_storage():
+    counter = session.get('counter', 0)
+    counter += 1
+    session['counter'] = counter
+
+    return str(counter)
 
 @app.route("/contact")
 def return_contacts():
-    res = requests.get(_url('/contacts?contact.id=28'))
+    #res = requests.get(_url('/contacts?contact.id=28'))
+    res = requests.get(_url('/contacts'))
     data = json.loads(res.text)
-    return str(data)
+    message = ("Hello {0}<br>".format('Alex'))
+    for c in data:
+        message += ('{name}<br>'.format_map(c['owner']))
+    return str(message)
 
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    #msg = request.form.get('Body')
     trasmisor = request.form.get('From')
     number = trasmisor.split(':')
-    user_json = _get('/users/?mobile={0}'.format(_phone('NAT',number[1])))
+
+    # busca el usuario por el telefono sin codigo del pais
+    user_json = _get('/users/?mobile={0}'.format(_phone('NAT', number[1])))
     if len(user_json) <= 0:
-        user_json = _get('/users/?mobile={0}'.format(_phone('INT',number[1])))
+        # busca el usuario por el telefono con codigo del pais
+        user_json = _get('/users/?mobile={0}'.format(_phone('INT', number[1])))
         if len(user_json) <= 0:
+            # no se encontro usuario
             user_json = False
+
     if user_json:
-        message = ("It's a pleasure talking to you {0}, how can I help you ?".format(
-            user_json[0]['name']))
+        # se encontro el usuario
+        contacts = _get('/contacts?contact.id={0}'.format(user_json[0]['id']))
+        #contacts = _get('/contacts?contact.id=28')
+        message = ("Hello {0}\nThe following contacts have memories for you:\n".format(user_json[0]['name']))
+
+        for c in contacts:
+            message += ('{name}\n'.format_map(c['owner']))
+        
+        message += ("Use the contact's name in a message to receive one of their memories")
+        
     else:
+        # no hay usuario
         message = (
             "Sorry we did not find your user :C, if you do not have an account you can create one in Heaven Sent")
     # Create reply
@@ -66,4 +91,4 @@ def _phone(type, number):
 
 
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(port=3001, debug=True)
