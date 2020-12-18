f3f54ae283d17f3754832637bbf61226956d974b
Will Alvarez <will.alvarez@gmail.com>
2020-12-17 18:13:40 -0600

    print values

--- HeavenSentBot/__init__.py
+++ HeavenSentBot/__init__.py
@@ -26,11 +26,80 @@ def create_app(test_config=None):
         return 'Hello'
 
     # chatbot
+    @app.route('/text', methods=['POST'])
+    def _main():
+        # get data
+        income_msg = request.values.get('Body', None)
+        trasmisor = request.values.get('From')
+        number = trasmisor.split(':')[1]
+        print(request.values)
+        #data = json.loads(request.data)
+        #number = data['number']
+        #income_msg = data['message']
+        # get session if not exist session equal 0
+        user_id = session.get('user_id', 0)
+        lang = session.get('lang', 'en')
+        # if session exist send to NLP
+        if user_id != 0:
+            # Noise removal
+            income_msg = str(normalize(income_msg))
+            flag = 0
+            for x in exit_terms:
+                if x in income_msg:
+                    message = logout.logout()
+                    flag = 1
+                    break
+
+            if not(flag):
+                if 'menu' in income_msg:
+                    message = ("{}\n".format(palabras[lang].get("menu_select")))
+                    message += flow_menu()
+                    flag = 1
+
+            if not(flag):
+                for x in help_terms:
+                    if x in income_msg:
+                        message = flow_help()
+                        flag = 1
+                        break
+
+            if not(flag):
+                flows = {
+                    "contacts": flow_contact,
+                    "memories": flow_memory,
+                    "language": change_lang,
+                    "phrases": chatbot
+                }
+                flow = session.get('thread', 'phrases')
+                func = flows.get(flow)
+                message = func(income_msg)
+
+        # if not send to login
+        else:
+            message = login.login(number)
+
+        response = MessagingResponse()
+        msg = Message()
+
+        if message[0] == 'media':
+            msg.media(message[1])
+        else:
+            msg.body(message)
+
+        response.append(msg)
+        # response
+        #resp = MessagingResponse()
+        # resp.message(message)g
+        # return str(resp)
+        return str(response)
+
+    # chatbot
     @app.route('/sms', methods=['POST'])
     def _main():
         # get data
         income_msg = request.form.get('Body')
         trasmisor = request.form.get('From')
+        
         number = trasmisor.split(':')[1]
         #data = json.loads(request.data)
         #number = data['number']
